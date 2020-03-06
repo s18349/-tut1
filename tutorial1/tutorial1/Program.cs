15653b20fb0089175b06d0e21dc71c7ec3ff6e55
s18349 <s18349@pjwstk.edu.pl>
2020-03-06 09:58:40 +0100

    Modified the project

--- tutorial1/tutorial1/Program.cs
+++ tutorial1/tutorial1/Program.cs
@@ -9,23 +9,43 @@ namespace tutorial1
     {
         public static async Task Main(String[] args)
         {
-            var websiteURl = args[0];
+            var websiteURl = args.Length > 0 ? args[0] : throw new ArgumentNullException();
+         
             var httpClinet = new HttpClient();
-            var response = await httpClinet.GetAsync(websiteURl);
-            if(response.IsSuccessStatusCode)
+            try
             {
-                var htmlContent = await response.Content.ReadAsStringAsync(); //asyncronise is always with await
-
-                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.+[a-z]", RegexOptions.IgnoreCase); //extract only email/ all captal treat as lowercase
-
-                var emailAdresses = regex.Matches(htmlContent);
-
-                foreach ( var emailAddress in emailAdresses)
+                var response = await httpClinet.GetAsync(websiteURl);
+                httpClinet.Dispose(); // after using httpclint we delete
+           
+                if (response.IsSuccessStatusCode)
                 {
-                    Console.WriteLine(emailAddress.ToString());
-                }
+                    var htmlContent = await response.Content.ReadAsStringAsync(); //asyncronise is always with await
 
+                    var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.+[a-z]", RegexOptions.IgnoreCase); //extract only email/ all captal treat as lowercase
+
+                    var emailAdresses = regex.Matches(htmlContent);
+
+                    if(emailAdresses.Count > 0)
+                    {
+                        foreach (var emailAddress in emailAdresses)
+                        {
+                            Console.WriteLine(emailAddress.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("no enmail adresses is found");
+                    }
+                    
+
+                }
             }
+            catch
+            {
+                Console.WriteLine("Error while downloading the page");
+            }
+
+           
 
             Console.ReadKey();
         }
