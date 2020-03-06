8837c17cb1207e33229829a718333079efb74dae
Jongok Yoon <s18349@pjwstk.edu.pl>
2020-03-06 09:30:21 +0100

    initial commit

---
+++ tutorial1/tutorial1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tutorial1
+{
+    public class Test
+    {
+        public static async Task Main(String[] args)
+        {
+            var websiteURl = args[0];
+            var httpClinet = new HttpClient();
+            var response = await httpClinet.GetAsync(websiteURl);
+            if(response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync(); //asyncronise is always with await
+
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.+[a-z]", RegexOptions.IgnoreCase); //extract only email/ all captal treat as lowercase
+
+                var emailAdresses = regex.Matches(htmlContent);
+
+                foreach ( var emailAddress in emailAdresses)
+                {
+                    Console.WriteLine(emailAddress.ToString());
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
